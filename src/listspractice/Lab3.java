beb66a14bab7af34ad5447d9a0e519b6ebf9253f
klwortman <klwortman@192.168.1.2>
2015-11-02 19:38:28 -0600

    Completed Lab 3

--- src/listspractice/Lab3.java
+++ src/listspractice/Lab3.java
@@ -16,5 +16,19 @@ public class Lab3 {
         employees.add(new Employee("Byrd", "Larry", "222-22-2222"));
         employees.add(new Dog("Hemi", 1234));
         employees.add(new Dog("Mopar", 5678));
+
+
+        for (int i = 0; i < employees.size(); i++) {
+            Object obj = employees.get(i);
+            Dog dog = null;
+            Employee employee = null;
+            
+            if (obj instanceof Dog) {
+                dog = (Dog) obj;
+            } else if (obj instanceof Employee) {
+                employee = (Employee) obj;
+            }
+            System.out.println(obj);
+        }
     }
 }
