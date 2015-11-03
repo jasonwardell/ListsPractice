8fa9b915fabc5ae42d903340e90ae0dcca89d5ac
klwortman <klwortman@192.168.1.2>
2015-11-02 19:28:37 -0600

    Completed Lab 2

--- src/listspractice/Lab2.java
+++ src/listspractice/Lab2.java
@@ -1,11 +1,24 @@
 package listspractice;
 
+import java.util.ArrayList;
+import java.util.List;
 /**
  *
  * @author jwardell
  */
 public class Lab2 {
+
     public static void main(String[] args) {
-        
+        Employee employee = null;
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee("Smith", "John", "111-11-1111"));
+        employees.add(new Employee("Byrd", "Larry", "222-22-2222"));
+        employees.add(new Employee("Rodgers", "Aaron", "333-33-3333"));
+
+        System.out.println("Employee Information");
+        for (Employee e : employees) {
+            System.out.println(e);
+        }
     }
 }
--- src/listspractice/Lab3.java
+++ src/listspractice/Lab3.java
@@ -1,9 +1,20 @@
 package listspractice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author jwardell
  */
 public class Lab3 {
-    
+
+    public static void main(String[] args) {
+
+        List employees = new ArrayList();
+        employees.add(new Employee("Smith", "John", "111-11-1111"));
+        employees.add(new Employee("Byrd", "Larry", "222-22-2222"));
+        employees.add(new Dog("Hemi", 1234));
+        employees.add(new Dog("Mopar", 5678));
+    }
 }
